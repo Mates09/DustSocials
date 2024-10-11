6b967dfd2c31db239bfbc207a896475eb3322d2c
Mates09 <matej.snirc1@gmail.com>
2024-10-12 01:23:49 +0200

    auth added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import mongoDB from "./db/connectMonofDB.js";
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
